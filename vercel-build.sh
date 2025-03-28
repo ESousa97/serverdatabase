fafbed55f03627d82b4250cf53f2f49e616d93f5
Sousa <114963739+ESousa97@users.noreply.github.com>
2025-03-28 20:22:58 -0300

    Fix.: Update vercel.json and vercel-build.js

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-# Atualiza os repositórios e instala o python3-distutils
-apt-get update && apt-get install -y python3-distutils
-# Executa a instalação dos pacotes e o rebuild do sqlite3
-npm install
-npm rebuild sqlite3 --build-from-source
+npm install && npm run build
+
