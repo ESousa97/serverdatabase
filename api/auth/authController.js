b2f2e4b24077e6d61e449861e97d6615478e96ca
Sousa <114963739+ESousa97@users.noreply.github.com>
2025-03-29 21:05:23 -0300

    Feat.: Adicionado e corrigido o banco de dados.

--- api/auth/authController.js
+++ api/auth/authController.js
@@ -8,7 +8,7 @@ exports.login = async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
     res.json({ accessToken });
